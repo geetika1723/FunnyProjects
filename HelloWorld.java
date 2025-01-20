c4547e845e9d7522e08dcee2e60a971f3d26f6bc
Geetika <geetikasethi1723@gmail.com>
2025-01-21 02:48:21 +0530

    Cats are lovely
    This works on my machine... probably not yours.

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 class Helloworld{
     public static void main(String args[])
     {
-        System.out.println("Added some pawsitivity to the code.");
+        System.out.println("Added some pawsitivity to the code");
     }
 }
